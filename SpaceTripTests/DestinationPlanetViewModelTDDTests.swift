1b836afb9d9d36af1bcadab2093c9682b7baddf2
Tanin Rojanapiansatith <sp_land@hotmail.com>
2020-04-26 12:46:32 +0100

    Ping, check for mecury easterEggEnabled

--- SpaceTripTests/DestinationPlanetViewModelTDDTests.swift
+++ SpaceTripTests/DestinationPlanetViewModelTDDTests.swift
@@ -84,10 +84,26 @@ class DestinationPlanetViewModelTDDTests: XCTestCase {
         
     }
     
-    func test_signOfLife_planetMercury() {
+    func test_signOfLife_planetMercury_easterEggDisabled() {
         
         // given
         let sut = createSystemUnderTest(easterEggEnabled: false)
+        
+        // when
+        let result = sut.signOfLife(on: .mercury)
+        
+        // then
+        XCTAssertEqual(result.count, 1)
+        if result.count == 1 {
+            XCTAssertEqual(result[0], mercuryLifeSign1)
+        }
+        
+    }
+    
+    func test_signOfLife_planetMercury_easterEggEnabled() {
+        
+        // given
+        let sut = createSystemUnderTest(easterEggEnabled: true)
         
         // when
         let result = sut.signOfLife(on: .mercury)

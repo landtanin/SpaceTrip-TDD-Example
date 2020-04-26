2590125a239f067672f516093a74d9c0e56da3e7
Tanin Rojanapiansatith <sp_land@hotmail.com>
2020-04-26 11:35:56 +0100

    Ping, step 2, new test which fails to force the correct use of easterEggEnabled

--- SpaceTripTests/DestinationPlanetViewModelTDDTests.swift
+++ SpaceTripTests/DestinationPlanetViewModelTDDTests.swift
@@ -68,4 +68,20 @@ class DestinationPlanetViewModelTDDTests: XCTestCase {
         
     }
     
+    func test_signOfLife_planetVenus_easterEggDisabled() {
+        
+        // given
+        let sut = createSystemUnderTest(easterEggEnabled: false)
+        
+        // when
+        let result = sut.signOfLife(on: .venus)
+        
+        // then
+        XCTAssertEqual(result.count, 1)
+        if result.count == 1 {
+            XCTAssertEqual(result[0], venusLifeSign1)
+        }
+        
+    }
+    
 }
